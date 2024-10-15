33a65bf39053c74e32718e6d42c71264793a3e8b
Bình Phương Hoàng <hereda.unknown@gmail.com>
2024-10-15 14:56:37 +0700

    initial DART prj

---
+++ bin/prj_0.dart
@@ -0,0 +1,6 @@
+import 'package:prj_0/prj_0.dart' as prj_0;
+import 'dart:io';
+void main(List<String> arguments) {
+  
+  
+}
---
+++ test/prj_0_test.dart
@@ -0,0 +1,8 @@
+import 'package:prj_0/prj_0.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
